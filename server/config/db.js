a8c4466e221f42556093521caa951f794a133a6b
unknown <yogeshkumar13>
2026-03-23 02:37:34 +0530

    redy-server-up-tp-date

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // host: process.env.DB_HOST,
-  host: process.env.DATABASE_URL,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
