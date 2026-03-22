c858d5aaa2029c477f82dc6f9c5cde79ae2ae114
unknown <yogeshkumar13>
2026-03-23 01:50:14 +0530

    server-ready

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
+  host: process.env.DATABASE_URL,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
