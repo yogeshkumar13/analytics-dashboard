224248d85ad714c6b9521154c882e9dc40d5b3d1
unknown <yogeshkumar13>
2026-03-23 02:52:35 +0530

    --------

--- server/server.js
+++ server/server.js
@@ -2,19 +2,26 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const app = express();
+
+// 🔥 DB import (IMPORTANT)
+require("./config/db");  // 👈 ye line add karo
+
 // Routes import
 const authRoutes = require("./routes/authRoutes");
 const analyticsRoutes = require("./routes/analyticsRoutes");
 
-const app = express();
-
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://analytics-dashboard-iota-beryl.vercel.app/", // production me specific domain rakh sakte ho
+}));
+
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
 // Test route
 app.get("/", (req, res) => {
   res.send("API running 🚀");
