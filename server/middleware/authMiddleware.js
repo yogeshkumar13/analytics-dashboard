4c457e1a2a766845b436f983d023b6552fa0759b
unknown <yogeshkumar13>
2026-03-23 18:19:40 +0530

    finally-server

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ msg: "No token" });
   }
+
+  // ✅ sirf token part nikal lo
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
